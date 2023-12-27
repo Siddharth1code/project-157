354184ab2afa0ef9b955504fb559e656468f0656
Siddharth1code <88075593+Siddharth1code@users.noreply.github.com>
2023-12-27 12:40:59 +0530

    Add files via upload

---
+++ comics.js
@@ -0,0 +1,85 @@
+AFRAME.registerComponent("tour", {
+    init: function () {
+      this.placesContainer = this.el;
+      this.createCards();
+    },
+  
+    createCards: function () {
+      const thumbNailsRef = [
+        {
+          id: "ironman",
+          title: "ironman",
+          url: "./assets/thumbnails/ironman.webp",
+        },
+        {
+          id: "spiderman",
+          title: "spiderman",
+          url: "./assets/thumbnails/spiderman.jpg",
+        },
+  
+        {
+          id: "moonknight",
+          title: "moonknight",
+          url: "./assets/thumbnails/moonknight.jpg",
+        },
+        {
+          id: "thor",
+          title: "thor",
+          url: "./assets/thumbnails/thor.jpeg",
+        },
+      ];
+      let prevoiusXPosition = -60;
+  
+      for (var item of thumbNailsRef) {
+        const posX = prevoiusXPosition + 25;
+        const posY = 10;
+        const posZ = -40;
+        const position = { x: posX, y: posY, z: posZ };
+        prevoiusXPosition = posX;
+  
+        // Border Element
+        const borderEl = this.createBorder(position, item.id);
+        // Thumbnail Element
+        const thumbNail = this.createThumbNail(item);
+        borderEl.appendChild(thumbNail);
+        // Title Text Element
+        const titleEl = this.createTitleEl(position, item);
+        borderEl.appendChild(titleEl);
+  
+        this.placesContainer.appendChild(borderEl);
+      }
+    },
+    
+    createBorder : function(position, id){
+      const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("id", id);
+      entityEl.setAttribute("visible", true);
+      entityEl.setAttribute("geometry", {premitive : "ring", radiusInner : 9, radiusOuter : 10});
+      entityEl.setAttribute("position", position);
+      entityEl.setAttribute("material", {color : "#0077CC", opacity : 1});
+  
+      return entityEl;
+    },
+  
+    createThumbNail : function(item){
+      const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("visible", true);
+      entityEl.setAttribute("geometry", {premitive : "circle", radius : 9});
+      entityEl.setAttribute("material", {src : item.url});
+  
+      return entityEl;
+    },
+  
+    createTitleEl : function(position, item){
+      const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("text", {font : "exo2bold", align : "center", width : 70, color : "#e65100", value : item.title});
+      const elPosition = position;
+      elPosition.y = -20;
+      entityEl.setAttribute("visible", true);
+      entityEl.setAttribute("position", elPosition);
+      
+      return entityEl;
+    },
+  
+  });
+  
